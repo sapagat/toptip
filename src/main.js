72b43c20843a74abd651a712561227704bfb9c64
Sam Payá <sapagat@gmail.com>
2017-10-15 21:14:06 +0200

    Make use of router for displaying main page

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import MainPage from './pages/MainPage'
 import TipsService from './services/TipsService'
 import Bus from './infrastructure/Bus'
@@ -8,8 +9,16 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new TipsService(Bus)
 
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: MainPage }
+  ]
+})
+
+Vue.use(VueRouter)
+
 new Vue({
   el: '#app',
-  template: '<MainPage/>',
-  components: { MainPage }
+  router,
+  template: '<router-view></router-view>'
 })
